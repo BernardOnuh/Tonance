1c5a0dc4597a9f3af0fc32188c83c907315f2d53
Bernard.O <114490070+BernardOnuh@users.noreply.github.com>
2024-08-20 22:45:03 +0100

    remove @

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const imagePath = (fileName) => path.join(__dirname, "images", fileName);
 
 bot.start((ctx) => {
   const username = ctx.from.username ? `@${ctx.from.username}` : ctx.from.first_name;
-  const message = `<b>Welcome, @${username}! 👋</b>\n\n🌐 Tonance is the next generation of decentralized DeFi application on Telegram. A centralized exchange offering a cross-chain interoperability and liquidity transfer protocol that allows truly decentralized transfer of assets between various blockchains.\n\n💰 Tonance is offering universal token xBTON access through gamification within a Telegram mini-app. Anyone can start mining xBTON tokens for free and completing campaigns for free refining machines.`;
+  const message = `<b>Welcome, ${username}! 👋</b>\n\n🌐 Tonance is the next generation of decentralized DeFi application on Telegram. A centralized exchange offering a cross-chain interoperability and liquidity transfer protocol that allows truly decentralized transfer of assets between various blockchains.\n\n💰 Tonance is offering universal token xBTON access through gamification within a Telegram mini-app. Anyone can start mining xBTON tokens for free and completing campaigns for free refining machines.`;
 
   ctx.replyWithPhoto(
     { source: imagePath("tonance.png") },
